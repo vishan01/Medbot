6b47337e986b9e434bc933b2dbd0d8ee00802813
ujwala <132659209+ujwala1720@users.noreply.github.com>
2024-03-01 22:48:44 +0530

    init repo

---
+++ bot.py
@@ -0,0 +1,50 @@
+import load as helper
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+import os
+import prompt
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+embeddings = helper.download_hugging_face_embeddings()
+
+pinecone.init(api_key=PINECONE_API_KEY,
+              environment=PINECONE_API_ENV)
+
+index_name="medical-bot"
+
+#Loading the index
+docsearch=Pinecone.from_existing_index(index_name, embeddings)
+
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+chain_type_kwargs={"prompt": PROMPT}
+
+llm=CTransformers(model="llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config={'max_new_tokens':512,
+                          'temperature':0.8})
+
+
+qa=RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True, 
+    chain_type_kwargs=chain_type_kwargs)
+
+
+msg=input()
+print(msg)
+print()
+
+result=qa({"query": input})
+print("Response : ", result["result"])
---
+++ store.py
@@ -0,0 +1,33 @@
+from load import load_pdf, text_split, download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone as PC
+from pinecone import Pinecone
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+#print(PINECONE_API_KEY)
+# print(PINECONE_API_ENV)
+
+extracted_data = load_pdf("/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+#Initializing the Pinecone
+pc = Pinecone(api_key=PINECONE_API_KEY)
+#index = pc.Index("medical-bot")
+
+#pinecone.init(api_key=PINECONE_API_KEY,
+              #environment=PINECONE_API_ENV)
+
+
+index_name="medical-bot"
+
+
+#Creating Embeddings for Each of The Text Chunks & storing
+docsearch=PC.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
