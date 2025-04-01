97e658611232ad88e1f4bd3d773332607c7dfc5c
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-04-01 22:30:42 +0530

    Add Firebase configuration to config.js

---
+++ frontend/src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APP_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_APP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_APP_MSGID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_APP_MEASUREMENTID
+};
