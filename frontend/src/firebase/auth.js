4bac800ef402c4027466a9085be3dbe1768e2bba
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-04-02 21:34:58 +0530

    Implement Google sign-in functionality and add placeholders for email sign-up and sign-in in auth.js

--- frontend/src/firebase/auth.js
+++ frontend/src/firebase/auth.js
@@ -1,4 +1,35 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./config";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export const SignInGoogle = ()=>{signInWithPopup(auth, provider)
+.then((result) => {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  const credential = GoogleAuthProvider.credentialFromResult(result);
+  const token = credential.accessToken;
+  // The signed-in user info.
+  const user = result.user;
+  // IdP data available using getAdditionalUserInfo(result)
+  // ...
+}).catch((error) => {
+  // Handle Errors here.
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  // The email of the user's account used.
+  const email = error.customData.email;
+  // The AuthCredential type that was used.
+  const credential = GoogleAuthProvider.credentialFromError(error);
+  // ...
+});
+}
+
+export const SignUpEmail = (email, password) => {
+
+}
+
+export const SignInEmail = (email, password) => {
+
+}
