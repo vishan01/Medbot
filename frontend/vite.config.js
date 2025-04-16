c569cc0b294e22280a3dec06bb02701c2413779f
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-04-16 23:32:53 +0530

    chore: update Tailwind CSS and related configurations
    - Updated @tailwindcss/vite to version 4.1.4 in package.json.
    - Added postcss.config.js to configure PostCSS with Tailwind CSS and Autoprefixer.
    - Created tailwind.config.js for Tailwind CSS configuration.
    - Removed Tailwind CSS plugin from vite.config.js as it is now handled in postcss.config.js.
    - Updated tailwindcss dependency to version 3.4.17 in devDependencies.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react()
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
